095a7ec129c4e692756ec8f43f74de44cc3224d3
Fabricio Jesus <96658554+fabriciojesus@users.noreply.github.com>
2024-09-07 15:01:01 -0300

    Update Program.cs
    Including endpoint to update the age

--- Program.cs
+++ Program.cs
@@ -2,23 +2,29 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/user",() => new {Name = "Jett", Age = 35});
-app.MapGet("/Addheader",(HttpResponse response) => response.Headers.Add("Teste", "Jett Player"));
+app.MapGet("/user",() => new { Name = "Jett", Age = 35 });
+app.MapGet("/Addheader", (HttpResponse response) => response.Headers.Add("Teste", "Jett Player"));
 
-app.MapGet("/Addreturn",(HttpResponse response) => {response.Headers.Add("teste","Reyna");
-return new {Name = "Reyna", Age = 927};
+app.MapGet("/Addreturn", (HttpResponse response) => {
+    response.Headers.Add("teste", "Reyna");
+    return new { Name = "Reyna", Age = 927 };
 });
-
 
 app.MapPost("/saveProduct", (Product product) => {
-    return product.Code + " - " + product.Name; 
+    return product.Code + " - " + product.Name;
 });
 
+// Endpoint PUT para alterar a idade do usuário
+app.MapPut("/updateUserAge", (int newAge) => {
+    // Lógica simples para modificar a idade
+    var user = new { Name = "Jett", Age = newAge };
+    return user;
+});
 
 app.Run();
-public class Product{
+
+public class Product
+{
     public string Code { get; set; }
     public string Name { get; set; }
 }
-    
-
