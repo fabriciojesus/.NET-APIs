8a9e82260d271dd7d03ef3f69750006affd49e51
Fabricio Jesus <96658554+fabriciojesus@users.noreply.github.com>
2024-09-07 14:52:55 -0300

    APIs C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/user",() => new {Name = "Jett", Age = 35});
+app.MapGet("/Addheader",(HttpResponse response) => response.Headers.Add("Teste", "Jett Player"));
+
+app.MapGet("/Addreturn",(HttpResponse response) => {response.Headers.Add("teste","Reyna");
+return new {Name = "Reyna", Age = 927};
+});
+
+
+app.MapPost("/saveProduct", (Product product) => {
+    return product.Code + " - " + product.Name; 
+});
+
+
+app.Run();
+public class Product{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
+    
+
